d0cb6d0d105f07bfb5680ca81b844255083bf7be
alberto.castellini <alberto.castellini@be055ff6-b097-11dd-9763-1166fc48c086>
2008-11-21 10:11:55 +0000

    Repository-b v3.0.1.0

--- trunk/Maris XDS Repository-b/http_connections/http_client_new.php
+++ trunk/Maris XDS Repository-b/http_connections/http_client_new.php
@@ -1,9 +1,16 @@
 <?php
-     //define ("CRLF", "\r\n");
-/*
- * Created on 11-nov-2005
- * Biasio
- */
+# ------------------------------------------------------------------------------------
+# MARIS XDS REGISTRY
+# Copyright (C) 2007 - 2010  MARiS Project
+# Dpt. Medical and Diagnostic Sciences, University of Padova - csaccavini@rad.unipd.it
+# This program is distributed under the terms and conditions of the GPL
+
+# Contributor(s):
+# A-thon srl <info@a-thon.it>
+# Alberto Castellini
+
+# See the LICENSE files for details
+# ------------------------------------------------------------------------------------
  class HTTP_Client
 {
 	var $host;
