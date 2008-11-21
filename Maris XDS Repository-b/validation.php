d56e64aface40dd29e4272f85f77b66350efa270
alberto.castellini <alberto.castellini@be055ff6-b097-11dd-9763-1166fc48c086>
2008-11-21 10:11:55 +0000

    Repository-b v3.0.1.0

--- Maris XDS Repository-b/validation.php
+++ Maris XDS Repository-b/validation.php
@@ -4,6 +4,11 @@
 # Copyright (C) 2007 - 2010  MARiS Project
 # Dpt. Medical and Diagnostic Sciences, University of Padova - csaccavini@rad.unipd.it
 # This program is distributed under the terms and conditions of the GPL
+
+# Contributor(s):
+# A-thon srl <info@a-thon.it>
+# Alberto Castellini
+
 # See the LICENSE files for details
 # ------------------------------------------------------------------------------------
 include_once($lib_path."domxml-php4-to-php5.php");
